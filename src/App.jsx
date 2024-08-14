fbf969cc33a17ee22c51e3151a73886a21b8a1d1
mayankprajapat01 <mayankprajapatcs@gmail.com>
2024-08-15 00:04:46 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ import SignIn from "./pages/Signin";
 import SingUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import PricePredictor from "./pages/PricePredictor";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/sign-up" element={<SingUp />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/price-predictor" element={<PricePredictor />}></Route>
       </Routes>
     </BrowserRouter>
   );
