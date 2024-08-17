23d1ba6e0078b8a3b2748e691af72b8bfca89f7b
mayankprajapat01 <mayankprajapatcs@gmail.com>
2024-08-17 22:12:15 +0530

    complete sign up page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/Signin";
-import SingUp from "./pages/SignUp";
+import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import PricePredictor from "./pages/PricePredictor";
@@ -15,7 +15,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
-        <Route path="/sign-up" element={<SingUp />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
         <Route path="/price-predictor" element={<PricePredictor />}></Route>
